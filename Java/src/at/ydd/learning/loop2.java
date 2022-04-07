7efc5180bede9c6c48ce7f0a24049a24d8135ad5
Ian <ianlee.felder@gmail.com>
2022-04-07 16:03:10 +0200

    While

--- Java/src/at/ydd/learning/loop2.java
+++ Java/src/at/ydd/learning/loop2.java
@@ -1,9 +1,9 @@
 package at.ydd.learning;
 
-public class loop1 {
+public class loop2 {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 1; i <=100; i++) {
+        for (int i = 2; i < 1000; i = i + 2) {
             sum += i;
         }
         System.out.println(sum);
---
+++ Java/src/at/ydd/learning/while1.java
@@ -0,0 +1,21 @@
+package at.ydd.learning;
+import java.util.Random;
+
+public class while1 {
+    public static void main(String[] args) {
+
+        boolean isfinished = false;,
+        int sum = 0;
+        Random random = new Random();
+        int randomNumber = 0;
+        while (!isfinished){
+            randomNumber = random.nextInt(10,30);
+                sum += randomNumber;
+            if (randomNumber == 15 || randomNumber == 25) {
+                isfinished = true;
+            };
+        }
+
+        System.out.println(sum);
+    }
+}
