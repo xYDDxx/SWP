ac37e1396ebd5afc28608f818c85a899468133ee
Ian <ianlee.felder@gmail.com>
2022-06-02 15:40:29 +0200

    Test

---
+++ Java/src/at/ydd/learning/test.java
@@ -0,0 +1,20 @@
+package at.ydd.learning;
+import java.util.Random;
+
+public class test {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int randomNumber1 = random.nextInt(1000);
+        System.out.println("Random Number 1: " + randomNumber1);
+
+        int randomNumber2 = random.nextInt(randomNumber1);
+        System.out.println("Random Number 2: " + randomNumber2);
+
+
+        for (int i=randomNumber1; i>=randomNumber2; i--) {
+            System.out.println(i);
+        }
+    }
+    }
+
---
+++ Java/src/at/ydd/learning/test2.java
@@ -0,0 +1,22 @@
+package at.ydd.learning;
+
+import java.util.Random;
+
+public class test2 {
+    public static void main(String[] args) {
+        boolean isfinished = false;
+        Random random = new Random();
+        int randomNumber = 0;
+        int sum = 0;
+
+        while (!isfinished){
+            randomNumber = random.nextInt(10);
+            sum += randomNumber;
+            if (randomNumber == 5) {
+                isfinished = true;
+            };
+        }
+        System.out.println("The sum of the random numbers are " + sum);
+
+    }
+}
