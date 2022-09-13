04597b5d5ae6fc885a64fa2f99ea420d4a942cff
Ian <ianlee.felder@gmail.com>
2022-09-13 11:18:32 +0200

    Test

---
+++ Java/src/at/ydd/learning/array1.java
@@ -0,0 +1,18 @@
+package at.ydd.learning;
+
+public class array1 {
+    public static void main(String[] args) {
+
+        int[] data = {9,5,84,6,21,7,1,5,84,3};
+
+
+
+        int sum = 0;
+        for (int i = 0; i < data.length; i++) {
+            sum += data[i];
+        }
+        System.out.println(sum);
+
+
+    }
+}
