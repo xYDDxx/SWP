d36cb46a6f924e07c317b2b73b875347889bf9ec
Ian <ianlee.felder@gmail.com>
2022-09-16 14:34:31 +0200

    Test

---
+++ Java/src/at/ydd/learning/array3.java
@@ -0,0 +1,16 @@
+package at.ydd.learning;
+
+import java.util.Arrays;
+
+public class array3 {
+    public static void main(String[] args) {
+        int[] number = {8,3,2,22,8,1};
+
+        Arrays.parallelSort(number, 0, 6);
+
+        for (int i : number) {
+            System.out.print(i);
+        }
+
+    }
+}
