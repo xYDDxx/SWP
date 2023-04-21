9e6a302a11462c8cd8b3c0edb8b50592030d39f8
Ian <ianlee.felder@gmail.com>
2023-04-21 13:35:44 +0200

    Geometric Figures finished

--- Java/src/at/ydd/learning/basics/objectOrientation/geometricFigures/Main.java
+++ Java/src/at/ydd/learning/basics/objectOrientation/geometricFigures/Main.java
@@ -3,5 +3,15 @@ package at.ydd.learning.basics.objectOrientation.geometricFigures;
 public class Main {
     public static void main(String[] args) {
 
+        Figure F1 = new Figure("Float1",8);
+        Circle C1 = new Circle("Circle1",4);
+        Rectangle Re1 = new Rectangle("Rectangle1",4,12);
+        Square S1 = new Square("Square1",6);
+        Dice D1 = new Dice("Dice1",5);
+
+        C1.getArea();
+        Re1.getArea();
+        S1.getArea();
+        D1.getArea();
     }
 }
